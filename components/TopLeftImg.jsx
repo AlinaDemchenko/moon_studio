fa22cd8ed4f65565b7ef7278e1c19ee331abdb6b
Alina Demchenko <demalina88@gmail.com>
2024-06-10 00:30:55 -0700

    top image fixed

--- components/TopLeftImg.jsx
+++ components/TopLeftImg.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function TopLeftImg() {
   return (
-    <div className="absolute top-0 left-0 z-10 w-[300px] h-[215px] xl:w-[400px] xl:h-[511px]">
+    <div className="absolute top-0 left-0 z-10 w-[300px] xl:w-[400px]">
       <Image src="/dust.png" width="419" height="536" alt="stardust" />
     </div>
   );
