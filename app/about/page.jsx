d206fa5821eb0c4425f67895b1dcd4ec2c7dea8e
Alina Demchenko <demalina88@gmail.com>
2024-07-15 13:33:41 -0700

    latets

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -96,7 +96,7 @@ function About() {
   const [index, setIndex] = useState(0);
 
   return (
-    <main className="h-full py-32 text-center xl:text-left">
+    <main className="h-full py-32 text-center xl:text-left   pt-[118px] md:pt-[168px] xl:pt-[132px]">
       <Galaxy />
       <motion.div
         variants={fadeIn("right", 1.2)}
@@ -108,7 +108,10 @@ function About() {
         <div className="xl:bg-[url('/about-bg.png')] xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute z-0 opacity-60"></div>
       </motion.div>
       <div className="container h-full flex flex-col mx-auto items-center xl:flex-row gap-x-6">
-        <div className="flex-1 flex-col flex justify-center"></div>
+        <div className="flex-1 flex-col flex justify-center">
+          <h2><span>MoonStudio:</span> Crafting Digital Excellence</h2>
+          <p>The best developers, designers, and SEO managers came together to bring your digital visions to life. Our team excels in creating innovative and effective web solutions tailored to your needs.</p>
+        </div>
         <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
             {aboutData.map((item, itemIndex) => {
@@ -131,14 +134,19 @@ function About() {
           <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
             {aboutData[index].info.map((item, itemIndex) => {
               return (
-                <div key={itemIndex} className="flex flex-1 flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60">
+                <div
+                  key={itemIndex}
+                  className="flex flex-1 flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
+                >
                   <div className="font-light mb-2 md:mb-0 ">{item.title}</div>
                   <div className="hidden xl:flex">- </div>
                   <div>{item.stage}</div>
                   <div className="flex gap-x-4">
                     {item.icons?.map((icon, itemIndex) => {
                       return (
-                        <div key={itemIndex} className="text-2xl text-white">{React.createElement(icon)}</div>
+                        <div key={itemIndex} className="text-2xl text-white">
+                          {React.createElement(icon)}
+                        </div>
                       );
                     })}
                   </div>
