c6a0b94670ce446e96047ac844a3ddc99e52d95f
Alina Demchenko <demalina88@gmail.com>
2024-07-15 20:17:04 -0700

    services components

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,7 +1,19 @@
+"use client";
+
+import ServiceSlider from "@/components/ServiceSlider";
+import Bulb from "@/components/Bulb";
+import Circles from "@/components/Circles";
+import { motion } from "framer-motion";
+import { fadeIn } from "@/variants";
+
 function Services() {
-    return (
-      <div>Services</div>
-    )
-  }
-  
-  export default Services
+  return (
+    <div className="h-full py-36 flex items-center">
+      <Circles />
+      <div className="mx-auto container"></div>
+      <Bulb />
+    </div>
+  );
+}
+
+export default Services;
---
+++ components/Bulb.jsx
@@ -0,0 +1,7 @@
+import Image  from "next/image";
+
+const Bulb = () => {
+    return <div><Image scr={} width={} height={} className="w-full h-full" alt=''/></div>;
+  };
+  
+  export default Bulb;
