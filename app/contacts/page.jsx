95612df8fe509fbdadf5e9e3b56bce48a69f7222
Alina Demchenko <demalina88@gmail.com>
2025-02-11 20:45:48 +0200

    padding contacts

--- app/contacts/page.jsx
+++ app/contacts/page.jsx
@@ -22,7 +22,7 @@ function Contacts() {
   });
 
   return (
-    <main className="page-padding-y py-32 h-full overflow-hidden lg:pt-36 lg:pb-28">
+    <main className="page-padding-y pt-28 pb-[70px] md:py-32 h-full overflow-hidden lg:pt-36 lg:pb-28">
             <Galaxy />
       <div className="page-padding-x mx-auto max-sm:px-3 text-center xl:text-left md:flex items-center justify-center h-full overflow-y-scroll">
         <div className="flex flex-col w-full h-full md:h-fit max-w-[700px]">
